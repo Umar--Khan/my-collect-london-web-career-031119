659156480d978a554becbc056c7a2a90a17ad1cf
Umar Khan <umarkhandev@gmail.com>
2019-03-01 23:11:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
     i = 0
-    test = []  
-    while i < collection.size      
-      test = yield(collection[i])                     
+    test = []
+    while i < collection.size
+      test = yield(collection[i])
       i = i + 1
-      puts test    
-    end      
+      puts test
+    end
 end
