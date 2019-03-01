d90e2df52b82271a5f4558a93812719314ae743f
Umar Khan <umarkhandev@gmail.com>
2019-03-01 23:14:10 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+    i = 0
+    arr = []
+    while i < collection.size
+      test = yield(collection[i])
+      i = i + 1
+      arr.push(test)
+    end
+    arr
+end
