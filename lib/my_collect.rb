69211cf31216eb14a797223459ac5f3542c127e9
Umar Khan <umarkhandev@gmail.com>
2019-03-01 21:59:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 
 def my_collect(collection)
     i = 0
+    arr = []
     while i < collection.size
     yield(collection[i])
+    arr.push collection[i]
     i += 1
     end
-  collection
+  arr
 end
